bcfe335295f307df46c84655ac3fca8b8d7b345b
vickizinha <vickalves85@gmail.com>
2026-04-14 16:06:16 -0300

    feat: add unit tests folders

---
+++ tests/Ecommerce.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Ecommerce.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Ecommerce.Integration.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
